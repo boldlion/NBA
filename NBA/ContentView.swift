9851a23f7dcabbc83a7b15a73668d24239022ad1
boldlion <boldlion1988@gmail.com>
2019-09-08 16:05:30 +0300

    Initial Commit

---
+++ NBA/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NBA
+//
+//  Created by Daniela Gerdzhikova on 8.09.19.
+//  Copyright © 2019 Bold Lion. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
